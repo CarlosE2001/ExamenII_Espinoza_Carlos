d39a80d0525e4bdc25038e273b86aaa5ab30c8e7
Carlos Espinoza <carlos.espinozaperaza@ucr.ac.cr>
2021-12-05 17:59:38 -0600

    About us view

--- Pizza4U/Controllers/HomeController.cs
+++ Pizza4U/Controllers/HomeController.cs
@@ -55,15 +55,8 @@ namespace Pizza4U.Controllers {
 
 
         public ActionResult About() {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact() {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
