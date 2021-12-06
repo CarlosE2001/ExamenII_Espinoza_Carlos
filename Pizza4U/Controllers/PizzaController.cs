ee8def897c682a81a4865384678ad1ab06e8291d
Carlos Espinoza <carlos.espinozaperaza@ucr.ac.cr>
2021-12-05 21:35:51 -0600

    Added the custom pizza

---
+++ Pizza4U/Controllers/PizzaController.cs
@@ -0,0 +1,37 @@
+﻿using Pizza4U.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pizza4U.Controllers
+{
+    public class PizzaController : Controller
+    {
+        JsonParser parser { get; set; }
+
+        public PizzaController() {
+            this.parser = new JsonParser();
+        }
+        public ActionResult CustomPizza()
+        {
+            List<string> ingredients = new List<string>();
+            List<string> extras = new List<string>();
+            dynamic jsonCollection = this.parser.ParseFromJSON("Ingredients.json");
+            foreach(var element in jsonCollection) {
+                string name = element.Name;
+                ingredients.Add(name);
+            }
+
+            jsonCollection = this.parser.ParseFromJSON("Extras.json");
+            foreach (var element in jsonCollection) {
+                string name = element.Name;
+                extras.Add(name);
+            }
+            ViewBag.Ingredients = ingredients;
+            ViewBag.Extras = extras;
+            return View();
+        }
+    }
+}
---
+++ Pizza4U/Handlers/JsonParser.cs
@@ -0,0 +1,99 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace Pizza4U.Handlers {
+    public class JsonParser {
+        private string[] ExtractRawContent(string fileName) {
+            return File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data_Files/" + fileName));
+        }
+
+        public string ParseRawJson(string[] content) {
+            string contentExtracted = "";
+            foreach (string line in content) {
+                contentExtracted += line + "\n";
+            }
+            return contentExtracted;
+        }
+
+        public dynamic ParseFromJSON(string fileName) {
+            dynamic parsedContent = "";
+            try {
+                string[] rawData = ExtractRawContent(fileName);
+                string contentReadyToParse = ParseRawJson(rawData);
+                parsedContent = JsonConvert.DeserializeObject(contentReadyToParse);
+            } catch (Exception e) {
+                string error = "Error while parsing JSON raw data \n" + e.ToString();
+                Debug.WriteLine(error);
+            }
+            return parsedContent;
+        }
+
+        public List<string> GetListFromString(string content) {
+            List<string> listFromContent = new List<string>();
+            string[] contentList = content.Split('|');
+            foreach (string contentInList in contentList) {
+                if (contentInList != "") {
+                    listFromContent.Add(contentInList.Replace("_", " "));
+                }
+            }
+            return listFromContent;
+        }
+
+        public string GetStringFromList(List<string> listContent) {
+            string content = "";
+
+            foreach (string element in listContent) {
+                content += element.Replace(" ", "_") + "|";
+            }
+
+            return content;
+        }
+
+        //public List<QuizModel> GetQuizzesFromJson(dynamic jsonCollection) {
+        //    List<QuizModel> quizzes = new List<QuizModel>();
+        //    foreach (var element in jsonCollection) {
+        //        quizzes.Add(new QuizModel {
+        //            Title = element.Title,
+        //            Description = element.Description,
+        //            Difficulty = element.Difficulty,
+        //            Link = element.Link
+        //        });
+        //    }
+        //    return quizzes;
+        //}
+
+
+        public bool WriteToJsonFile<Model>(string fileName, Model model, Func<dynamic, List<Model>> GetModelsFromJson) {
+            bool success = false;
+            string jsonString = JoinNewData<Model>(fileName, model, GetModelsFromJson);
+            try {
+                File.WriteAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data_Files/" + fileName), jsonString);
+                success = true;
+            } catch {
+                Debug.WriteLine("Error occurred");
+            }
+            return success;
+        }
+
+        public string JoinNewData<Model>(string fileName, Model model, Func<dynamic, List<Model>> GetModelsFromJson) {
+            string resultingJson = "";
+            try {
+                string[] rawJson = ExtractRawContent(fileName);
+                string json = ParseRawJson(rawJson);
+                dynamic jsonCollection = JsonConvert.DeserializeObject(json);
+                List<Model> previousModels = GetModelsFromJson(jsonCollection);
+
+                previousModels.Add(model);
+                resultingJson = JsonConvert.SerializeObject(previousModels);
+            } catch {
+                Debug.WriteLine("Error occurred");
+            }
+            return resultingJson;
+        }
+    }
+}
