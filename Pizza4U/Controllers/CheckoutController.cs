fb8ae2cda94781e3af1b11e709d11dd7a67abd23
Carlos Espinoza <carlos.espinozaperaza@ucr.ac.cr>
2021-12-05 23:27:59 -0600

    Added payment

---
+++ Pizza4U/Controllers/CheckoutController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pizza4U.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        // GET: Checkout
+        public ActionResult Checkout()
+        {
+            CookieController cookies = new CookieController();
+            return View();
+        }
+    }
+}
